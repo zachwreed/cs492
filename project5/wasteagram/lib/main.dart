a6bca5c2707e686d83cbeef89b33e70a3c710a69
Zach Reed <reedz@oregonstate.edu>
2020-08-12 22:00:55 -0500

    working sentry

--- project5/wasteagram/lib/main.dart
+++ project5/wasteagram/lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:wasteagram/widgets/app.dart';
+import 'package:flutter/foundation.dart' as Foundation;
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:async';
 
-void main() {
-  runApp(WasteagramApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  String sentryURL = await rootBundle.loadString("assets/dsn.txt");
+
+  Foundation.FlutterError.onError = (Foundation.FlutterErrorDetails details) {
+    Zone.current.handleUncaughtError(details.exception, details.stack);
+  };
+
+  runZoned(() {
+    runApp(WasteagramApp());
+  }, onError: (error, stackTrace) {
+    WasteagramApp.reportError(error, stackTrace, sentryURL);
+  });
 }
--- project5/wasteagram/lib/widgets/app.dart
+++ project5/wasteagram/lib/widgets/app.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:wasteagram/screens/detail_screen.dart';
 import 'package:wasteagram/screens/list_screen.dart';
 import 'package:wasteagram/screens/new_post_screen.dart';
+import 'package:flutter/foundation.dart' as Foundation;
+import 'package:sentry/sentry.dart';
 
 class WasteagramApp extends StatelessWidget {
   static final routes = {
@@ -12,10 +14,25 @@ class WasteagramApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // throw StateError('Test Error');
     return MaterialApp(
       title: 'Wasteagram',
       theme: ThemeData.dark(),
       routes: routes,
+    );
+  }
+
+  static Future<void> reportError(
+      dynamic error, dynamic stackTrace, String sentryURL) async {
+    // if (Foundation.kDebugMode) {
+    //   print(stackTrace);
+    //   return;
+    // }
+
+    final SentryClient sentry = SentryClient(dsn: sentryURL);
+    final SentryResponse response = await sentry.captureException(
+      exception: error,
+      stackTrace: stackTrace,
     );
   }
 }
